b400f4f86c210d329c914f411c1eb5c479693c57
TILTNES Bendik <btiltnes@one.steria.dom>
2019-11-15 16:42:16 +0100

    Removing commented out line in server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,6 @@ const port = 5000;
 app.use(express.static('public'));
 app.use(cors());
 app.use(bodyParser.json());
-//app.use('/api', api);
 app.use(api);
 
 app.listen(port, () => console.log('Example app listening on port %s!', port))
